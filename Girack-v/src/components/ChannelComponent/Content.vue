2e6612e76c913ff11f6c9f762905a56bb6e33247
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-02-26 20:57:26 +0900

    [add] - メッセージを表示できるように
    受け取りはまだ

--- Girack-v/src/components/ChannelComponent/Content.vue
+++ Girack-v/src/components/ChannelComponent/Content.vue
@@ -5,11 +5,40 @@ const socket = getSocket();
 export default {
     data() {
         return {
-            msg: "fdsa",
-            s: socket,
-            sStatus: false
+            msgDB: {
+                "001": [
+                    {
+                        id: 0,
+                        username: "Test",
+                        userid: "xxx",
+                        channelid: "001",
+                        msg: ["Ayo", "abc", "そしてこれが３つ目"]
+                    },
+                    {
+                        id: 0,
+                        username: "Admin",
+                        userid: "xxx",
+                        channelid: "001",
+                        msg: ["は", "誰お前"]
+                    }
+                ]
+            }
 
         }
+    },
+
+    mounted() {
+        //メッセージ受け取り
+        socket.on("msgReceive", (msg) => {
+            this.msgDB += this.msgDB.push({
+                username: msg.username,
+                userid: msg.userid,
+                channelid: msg.channelid,
+                msg: msg.content
+            });
+
+        });
+
     }
 
 }
@@ -17,6 +46,12 @@ export default {
 </script>
 
 <template>
-    <p>これがチャンネル :: {{ $route.params.id }}</p>
-    {{ s.connected }}
+    <div v-for="m in msgDB[$route.params.id]">
+        <v-card class="rounded-lg" variant="tonal" elevation="4" style="margin:3% auto; width:95%; padding:3% 1%;">
+            <div :class="'text-h6'">{{ m.username }}</div>
+            <p style="margin:1% 1% 0 1%" v-for="conte in m.msg">
+                {{ conte }}
+            </p>
+        </v-card>
+    </div>
 </template>
