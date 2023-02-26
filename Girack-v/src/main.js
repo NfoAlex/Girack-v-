c42dc861d1440870649a0780ff7a6a3182fe4714
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-02-26 20:57:03 +0900

    [fix] - アイコンが表示できていなかった

--- Girack-v/src/main.js
+++ Girack-v/src/main.js
@@ -8,6 +8,10 @@ import colors from 'vuetify/lib/util/colors'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, fa } from 'vuetify/iconsets/fa'
+import {mdi} from "vuetify/lib/iconsets/mdi"
+import '@mdi/font/css/materialdesignicons.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 const vuetify = createVuetify({
     theme: {
@@ -30,6 +34,14 @@ const vuetify = createVuetify({
             },
         },
         defaultTheme: "dark"
+    },
+    icons: {
+        defaultSet: "fa",
+        iconfont: "mdi",
+        aliases,
+        sets: {
+            mdi, fa
+        }
     },
     components,
     directives,
