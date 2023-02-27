8642df806859966764096ecfe11ebb31f10ecbe3
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-02-28 04:35:20 +0900

    [change] - ユーザーページのCSS調整

--- Girack-v/src/components/User.vue
+++ Girack-v/src/components/User.vue
@@ -28,17 +28,17 @@ export default {
 
 <template>
     <p>ToDo</p>
-    <div class="mx-auto" style="width:90%; margin-top:5%; height:90%;">
+    <div class="mx-auto" style="width:90%; margin-top:5%; height:90%; overflow-y:auto;">
         <v-container class="bg-surface-variant">
             <v-row no-gutters>
-                <v-col cols="3">
+                <v-col cols="2">
                     <v-card variant="tonal" :class="cd" style="padding:0">
                         <v-img class="rounded-lg" :alt="userinfo.userid" :src="'http://localhost:33333/img/' + userinfo.userid + '.jpeg'"></v-img>
                     </v-card>
                 </v-col>
                 <v-col>
                     <div variant="tonal" :class="cd" style="padding:1% 10% ">
-                        <p color="secondary" class="text-left text-overline">
+                        <p class="text-left text-h6">
                             # {{ userinfo.userid }}
                         </p>
                         <p class="text-h4 text-left" >
@@ -50,24 +50,25 @@ export default {
         </v-container>
         <v-container class="bg-surface-variant">
             <v-row no-gutters>
-                <v-card variant="tonal" :class="cd" style="width:100%; ">
-                    <p class="text--primary text-left" >
+                <v-card variant="tonal" :class="cd" style="width:100%;">
+                    <p class="text--primary text-h6 text-left" >
                         参加しているチャンネル
                     </p>
-                    <v-card
-                        v-for="c in channelIndex"
-                        class="mx-auto text-left"
-                        :class="cd"
-                        max-width="95%"
-                        style="margin-top:15px"
-                        :elevation="6"
-                    >
-                        <span class="text-h6" style="border-right:0.1px">{{ c.channelname }}</span>
-                        <span style="height:100%; border-right:1px solid grey; margin:0 1%"></span>
-                        <v-chip>公開</v-chip>
-                        <span style="height:100%; border-right:1px solid grey; margin:0 1%"></span>
-                        <span>{{ c.description }}</span>
-                    </v-card>
+                    <div style="overflow-y:scroll !important; max-height:40vh">
+                        <v-card
+                            v-for="c in channelIndex"
+                            class="mx-auto text-left"
+                            max-width="95%"
+                            style="margin-top:15px; height:30%; padding:8px 3%;"
+                            :elevation="6"
+                        >
+                            <span class="text-h6" style="border-right:0.1px">{{ c.channelname }}</span>
+                            <span style="height:100%; border-right:1px solid grey; margin:0 1%"></span>
+                            <v-chip>{{ c.scope==="public"?"公開":"非公開" }}</v-chip>
+                            <span style="height:100%; border-right:1px solid grey; margin:0 1%"></span>
+                            <span>{{ c.description }}</span>
+                        </v-card>
+                    </div>
                 </v-card>
             </v-row>
         </v-container>
@@ -101,7 +102,6 @@ export default {
 .card-default
 {
     padding: 3%;
-
     text-align:center;
 }
 
