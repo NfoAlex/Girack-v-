c1d26f76b5522f48c4e6f84b5c3755e6f717d52f
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-02-27 23:25:49 +0900

    [add] - ヘッダにチャンネル名と概要を表示するように

--- Girack-v/src/components/ChannelComponent/Head.vue
+++ Girack-v/src/components/ChannelComponent/Head.vue
@@ -1,8 +1,45 @@
+<script setup>
+import { getSocket, channelIndex } from '../../socket';
+const socket = getSocket();
+</script>
+
 <script>
+
+export default {
+
+    data() {
+        channelinfo: {}
+    },
+
+    computed: {
+        getPath() {
+            return this.$route.params.id;
+        }
+    },
+
+    methods: {
+        getChannelInfo() {
+            return channelIndex[this.getPath];
+
+        }
+    },
+
+    mounted() {
+        // this.channelname = channelIndex[this.getPath].channelname;
+        console.log("HEAD");
+        console.log(this.channelname);
+        console.log(channelIndex);
+    }
+
+}
+
 </script>
 
 <template>
-    <div :class="'text-h4'">{{ $route.params.id }}</div>
+    <div style="padding: 1% 32px">
+        <div :class="'text-h4'">{{ getChannelInfo().channelname }}</div>
+        <p>{{ getChannelInfo().description }}</p>
+    </div>
 </template>
 
 <style scoped>
