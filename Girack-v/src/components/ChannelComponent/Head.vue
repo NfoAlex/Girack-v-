a708a561e1ce8e3354f27e222fea42aca63bb302
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-03-07 20:24:03 +0900

    [add] - ヘッダーでプライベートなら鍵マークをつけるようにした
    その他作業進捗

--- Girack-v/src/components/ChannelComponent/Head.vue
+++ Girack-v/src/components/ChannelComponent/Head.vue
@@ -19,17 +19,30 @@ export default {
             console.log("getChannelInfo :: ");
             console.log(channelIndex[this.getPath]);
             try {
+                //チャンネルインデックスから情報を返す、データなければホームに返す
                 if ( channelIndex[this.getPath] !== undefined ) {
                     return channelIndex[this.getPath];
 
                 } else {
-                    location.pathname = "/home";
+                    setTimeout(this.$forceUpdate(), 1000);
+                    return {
+                        channelname: "ロード中...",
+                        description: "",
+                        scope: "open"
+                    }
+                    //location.pathname = "/home";
 
                 }
             }
             catch(e) {
-                location.pathname = "/";
-                return null;
+                setTimeout(this.$forceUpdate(), 1000);
+                return {
+                    channelname: "ロード中...",
+                    description: "",
+                    scope: "open"
+                }
+                //location.pathname = "/";
+                //return null;
             }
 
         }
@@ -48,10 +61,17 @@ export default {
 </script>
 
 <template>
-    <div style="padding: 1% 32px">
-        <div :class="'text-h4'">{{ getChannelInfo().channelname }}</div>
+    <div style="padding: 1% 32px; float:left;">
+        <div :class="'text-h4'">
+            <span v-if="getChannelInfo().scope==='private'" class="mdi mdi-lock"></span>
+            {{ getChannelInfo().channelname }}
+        </div>
         <p>{{ getChannelInfo().description }}</p>
     </div>
+    <div style="width:40%; float:right; padding: 0" class="d-flex flex-row-reverse">
+        <v-btn class="rounded-lg" variant="text" icon="" style="height:70px; width:70px;">履歴↑</v-btn>
+    </div>
+    
 </template>
 
 <style scoped>
