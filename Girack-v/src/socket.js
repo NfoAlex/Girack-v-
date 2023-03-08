72894be45e6e90b30581cdedeef46d8658e18ff5
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-03-09 02:42:56 +0900

    [change/add] - データの更新、受信方法を大幅に変更

--- Girack-v/src/socket.js
+++ Girack-v/src/socket.js
@@ -7,6 +7,19 @@ import { io } from 'socket.io-client'; //ウェブソケット通信用
 export const backendURI = "http://" + location.hostname + ":33333";
 const socket = io(backendURI);
 
+//他のコンポーネントで情報が更新されたかどうかを管理
+export var updateState = {
+    userinfo: 0,
+    serverinfo: 0,
+    channelinfo: 0
+}
+
+//情報とったとやるだけ
+export function updateStateTaken(i) {
+    updateState[i] = updateState[i] - 1;
+
+}
+
 //ユーザー情報
 export var userinfo = {
     username: null, //名前
@@ -57,10 +70,8 @@ export var msgDBbackup = {
     // ]
 };
 
+//ユーザー情報
 export var userIndexBackup = {
-    "xx0": {username:"Test", role:"Member"},
-    "xx1": {username:"Admin", role:"Member"},
-    "xx2": {username:"Psyonix", role:"Member"}
 };
 
 //ソケットの接続状態をもつオブジェクトを返すだけ
@@ -83,7 +94,7 @@ export function getMessage(channelid, readLength) {
 
 }
 
-//ユーザー情報返す
+//ユーザー情報返すだけ
 export function getUserinfo() {
     return userinfo;
 
@@ -113,6 +124,8 @@ socket.on("serverinfo", (dat) => {
         inviteOnly: dat.inviteOnly //招待オンリーかどうか
     };
 
+    updateState.serverinfo = true;
+
 });
 
 //情報受け取り
@@ -121,16 +134,18 @@ socket.on("infoResult", (dat) => {
     console.log(dat);
 
     //データがチャンネル用なら
-    if ( dat.type === "channel" ) {
-        console.log("channelIndexを更新します");
-        //チャンネル用情報JSONへ追加
-        channelIndex[dat.channelid] = {
-            channelname: dat.channelname, //チャンネル名
-            description: dat.description, //チャンネル概要
-            scope: dat.scope //チャンネルの公開範囲
-        };
+    // if ( dat.type === "channel" ) {
+    //     console.log("channelIndexを更新します");
+    //     //チャンネル用情報JSONへ追加
+    //     channelIndex[dat.channelid] = {
+    //         channelname: dat.channelname, //チャンネル名
+    //         description: dat.description, //チャンネル概要
+    //         scope: dat.scope //チャンネルの公開範囲
+    //     };
 
-    }
+    //     updateState.channelinfo = true;
+
+    // }
 
     //データがサーバー用なら
     if ( dat.type === "server" ) {
@@ -216,6 +231,90 @@ socket.on("infoResult", (dat) => {
 
 });
 
+//チャンネル情報の更新
+socket.on("infoChannel", (dat) => {
+    console.log("socket :: infoChannel : チャンネル情報更新");
+
+    channelIndex[dat.channelid] = {
+        channelname: dat.channelname, //チャンネル名
+        description: dat.description, //チャンネル概要
+        scope: dat.scope //チャンネルの公開範囲
+    };
+
+    updateState.channelinfo = updateState.channelinfo + 1;
+
+    console.log("channelIndex :: " + updateState.channelinfo);
+    console.log(channelIndex);
+
+});
+
+//プロフィール情報の受け取り
+socket.on("infoUser", (dat) => {
+    //自分の情報の更新にだけ使うから
+    if ( dat.userid !== userinfo.userid ) { return; }
+
+    console.log("infoUser :: プロフィール更新");
+
+    //参加しているチャンネルリストの長さ比較
+    if ( dat.channelJoined.length !== userinfo.channelJoined.length ) {
+        //チャンネル数が増えているなら
+        if ( dat.channelJoined.length > userinfo.channelJoined.length ) {
+            let channelNew = (dat.channelJoined).filter( cid => !(userinfo.channelJoined).includes(cid) );
+            
+            console.log("socket :: チャンネル差 : ");
+            console.log(channelNew);
+
+            //チャンネル情報の取得
+            for ( let c in channelNew ) {
+                socket.emit("getInfoChannel", { //リクエスト送信
+                    targetid: channelNew[c],
+                    reqSender: {
+                        userid: userinfo.userid, //ユーザーID
+                        sessionid: userinfo.sessionid //セッションID
+                    }
+                });
+
+            }
+
+        }
+
+        //チャンネル数が減っている（チャンネルを抜けた）なら
+        if ( dat.channelJoined.length < userinfo.channelJoined.length ) {
+            console.log("socket :: infoResult : チャンネル差が少ないから減らす");
+            dat.channelid = userinfo.channelJoined.filter(cid => !dat.channelJoined.includes(cid));
+
+            console.log("socket :: infoResult : 今参加しているチャンネル -> " + dat.channelJoined);
+            //自分が抜けたチャンネル分channelIndexを削る
+            for (let c=0; c<Object.keys(channelIndex).length; c++ ) {
+                let channelid = Object.keys(channelIndex)[c];
+                console.log("socket :: infoResult : 使うチャンネルID -> " + channelid);
+                
+                //チャンネルIDがユーザーが参加しているチャンネルIDリストに入っているかどうか調べる
+                if ( !dat.channelJoined.includes(channelid) ) {
+                    delete channelIndex[channelid]; //そのチャンネルIDのJSONを削除
+                    console.log("socket :: infoResult : 削除された!");
+                    break;
+
+                }
+
+            }
+
+        }
+
+    }
+
+    //個人情報更新
+    userinfo = {
+        username: dat.username,
+        userid: userinfo.userid, //ユーザーID
+        role: dat.role, //ロール
+        loggedin: true, //ログイン状態はそのまま
+        sessionid: userinfo.sessionid, //セッションIDはそのまま
+        channelJoined: dat.channelJoined, //参加しているチャンネル
+    }
+
+});
+
 //メッセージの履歴受け取り
 socket.on("messageHistory", (history) => {
     console.log("messageResult :: history ↓");
@@ -294,13 +393,20 @@ socket.on("authResult", (dat) => {
     if ( dat.result ) { //もしログイン成功なら
         //ユーザー情報を更新
         userinfo = {
-            username: dat.username,
             userid: dat.userid, //ユーザーID
-            role: dat.role,
             loggedin: true, //ログイン状態
             sessionid: dat.sessionid, //セッションID
-            channelJoined: dat.channelJoined, //参加しているチャンネル
-        }
+            channelJoined: dat.channelJoined
+        };
+
+        socket.emit("getInfoUser", {
+            targetid: dat.userid,
+            reqSender: {
+                userid: dat.userid,
+                sessionid: dat.sessionid
+            },
+
+        });
 
         setCookie("sessionid", dat.sessionid, 15);
         console.log("session id in cookie -> " + getCookie("sessionid"));
@@ -310,8 +416,7 @@ socket.on("authResult", (dat) => {
 
         //チャンネル情報の取得
         for ( let c in userinfo.channelJoined ) {
-            socket.emit("getInfo", { //リクエスト送信
-                target: "channel",
+            socket.emit("getInfoChannel", { //リクエスト送信
                 targetid: userinfo.channelJoined[c],
                 reqSender: {
                     userid: userinfo.userid, //ユーザーID
