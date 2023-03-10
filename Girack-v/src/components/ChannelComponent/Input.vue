75b9ea6b60ff0ce949d3270363f071fef635170b
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-03-10 18:31:53 +0900

    [change] - ref型データでのメッセージ送信に対応

--- Girack-v/src/components/ChannelComponent/Input.vue
+++ Girack-v/src/components/ChannelComponent/Input.vue
@@ -1,9 +1,10 @@
 <script setup>
-import { getUserinfo, channelIndex, getSocket } from '../../socket.js';
+import { dataUser, channelIndex, getSocket } from '../../socket.js';
 </script>
 
 <script>
 const socket = getSocket();
+const { Userinfo } = dataUser();
 
 export default {
     data() {
@@ -17,9 +18,9 @@ export default {
         //メッセージを送信する
         msgSend() {
             socket.emit("msgSend", {
-                userid: getUserinfo().userid, //名前
+                userid: Userinfo.value.userid, //名前
                 channelid: this.getPath, //チャンネルID
-                sessionid: getUserinfo().sessionid, //セッションID);
+                sessionid: Userinfo.value.sessionid, //セッションID);
                 content: this.txt
             });
             this.txt = ""; //入力欄を空に
