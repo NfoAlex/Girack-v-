6b85c7459b05798ca99406366299991aa113b501
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-03-21 21:18:22 +0900

    [change] - ルーター上のメニューページの名前を変更

--- Girack-v/src/router/index.js
+++ Girack-v/src/router/index.js
@@ -19,7 +19,7 @@ const router = createRouter({
     },
     {
       path: '/menu/',
-      name: 'User Page',
+      name: 'Menu',
       component: () => import('../components/Menu.vue'),
       children: [
         {
