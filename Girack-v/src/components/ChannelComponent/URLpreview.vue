232cd7c14d260d99e9a3f738b230c689c11ebc5c
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-03-25 03:10:43 +0900

    [fix] - URLがなかった時、表示を隠して空欄表示を無くした

--- Girack-v/src/components/ChannelComponent/URLpreview.vue
+++ Girack-v/src/components/ChannelComponent/URLpreview.vue
@@ -13,61 +13,23 @@ export default {
 </script>
 
 <template>
-    <div
-        style="height:fit-content; max-height:400px; max-width:800px; width:95%; margin-top:8px; overflow-y:scroll"
-        color="#222"
-        class="overflow-y-hidden d-flex flex-row"
-        v-for="link in urlData.data"
-    >
-
-        <!-- ウェブサイト用 -->
-        <v-card
-            v-if="link.mediaType==='website' && (link.title!=='')"
-            style="width:80%; overflow-y:scroll;"
+    <div v-for="link in urlData.data">
+        <div
+            style="height:fit-content; max-height:400px; max-width:800px; width:95%; margin-top:8px; overflow-y:scroll"
             color="#222"
-            class="pa-3 rounded-lg"
+            class="overflow-y-hidden d-flex flex-row"
+            v-if="link.title!==''"
         >
-            <div class="d-flex flex-row align-center">
-                <v-avatar class="rounded-lg" style="margin:16px 4px; float:left;" :image="link.favicon" size="32">
-                </v-avatar>
-                
-                <!-- 記事のタイトル -->
-                <p class="text-subtitle-1">
-                    <a :href="link.url" target="_blank">
-                        {{ link.title }}
-                    </a>
-                </p>
-            </div>
 
-            <!-- 記事の概要 -->
-            <p v-if="link.description" class="text-subtitle-2 ma-3 text-medium-emphasis">
-                {{ link.description }}
-            </p>
-        </v-card>
-
-        <!-- 記事あるいは画像メインの何か用 -->
-        <v-card
-            v-if="link.mediaType==='article' && (link.title!=='')"
-            style="width:100%; overflow-y:scroll;"
-            color="#222"
-            class="pa-3 rounded-lg d-flex flex-row"
-        >
-            <v-img
-                style="max-height:300px; min-width:30%;"
-                v-if="link.img"
-                :src="link.img"
+            <!-- ウェブサイト用 -->
+            <v-card
+                v-if="link.mediaType==='website' && (link.title!=='')"
+                style="width:80%; overflow-y:scroll;"
+                color="#222"
+                class="pa-3 rounded-lg"
             >
-            </v-img>
-
-            <div class="d-flex flex-column">
-                <div style="margin-left: 16px;" class="d-flex flex-row align-center">
-
-                    <v-avatar
-                        class="rounded-lg"
-                        style="margin:4px 4px;" 
-                        :image="link.favicon"
-                        size="24"
-                    >
+                <div class="d-flex flex-row align-center">
+                    <v-avatar class="rounded-lg" style="margin:8px 4px; float:left;" :image="link.favicon" size="32">
                     </v-avatar>
                     
                     <!-- 記事のタイトル -->
@@ -76,30 +38,70 @@ export default {
                             {{ link.title.length>100 ? link.title.substring(0,100)+"..." : link.title }}
                         </a>
                     </p>
-
                 </div>
 
                 <!-- 記事の概要 -->
-                <p class="text-body-2 ma-3 font-weight-light text-medium-emphasis">
-                    {{ link.description.length>135 ? link.description.substring(0,135)+"..." : link.description.length }}
+                <p v-if="link.description" class="text-body-2 ma-1 font-weight-light text-medium-emphasis">
+                    {{ link.description.length>135 ? link.description.substring(0,135)+"..." : link.description }}
                 </p>
+            </v-card>
 
-            </div>
-
-        </v-card>
-
-        <!-- 画像単体用 -->
-        <v-card
-            v-if="link.mediaType==='image'"
-            class="rounded-lg pa-2"
-            color="#222"
-            style="width:fit-content;"
-        >
-            <div class="mx-auto">
-                <v-img style="margin:8px 4px; width:auto; min-width:150px; max-height:300px;" :src="link.url">
+            <!-- 記事あるいは画像メインの何か用 -->
+            <v-card
+                v-if="link.mediaType==='article' && (link.title!=='')"
+                style="width:100%; overflow-y:scroll;"
+                color="#222"
+                class="pa-3 rounded-lg d-flex flex-row"
+            >
+                <v-img
+                    style="max-height:300px; min-width:30%;"
+                    v-if="link.img"
+                    :src="link.img"
+                >
                 </v-img>
-            </div>
-        </v-card>
 
+                <div class="d-flex flex-column">
+                    <div style="margin-left: 16px;" class="d-flex flex-row align-center">
+
+                        <v-avatar
+                            class="rounded-lg"
+                            style="margin:4px 4px;" 
+                            :image="link.favicon"
+                            size="24"
+                        >
+                        </v-avatar>
+                        
+                        <!-- 記事のタイトル -->
+                        <p class="text-subtitle-2">
+                            <a :href="link.url" target="_blank">
+                                {{ link.title.length>100 ? link.title.substring(0,100)+"..." : link.title }}
+                            </a>
+                        </p>
+
+                    </div>
+
+                    <!-- 記事の概要 -->
+                    <p class="text-body-2 ma-3 font-weight-light text-medium-emphasis">
+                        {{ link.description.length>135 ? link.description.substring(0,135)+"..." : link.description }}
+                    </p>
+
+                </div>
+
+            </v-card>
+
+            <!-- 画像単体用 -->
+            <v-card
+                v-if="link.mediaType==='image'"
+                class="rounded-lg pa-2"
+                color="#222"
+                style="width:fit-content;"
+            >
+                <div class="mx-auto">
+                    <v-img style="margin:8px 4px; width:auto; min-width:150px; max-height:300px;" :src="link.url">
+                    </v-img>
+                </div>
+            </v-card>
+
+        </div>
     </div>
 </template>
