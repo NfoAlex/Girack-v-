b721c28f2853261ac1231f860e09de00f24da5ce
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-03-26 07:01:27 +0900

    [change] - ビルド番号を書いておく

--- Girack-v/src/components/MenuComponent/AboutGirack.vue
+++ Girack-v/src/components/MenuComponent/AboutGirack.vue
@@ -5,8 +5,8 @@
     <div class="text-center pa-5" style="width:80%">
         <p class="text-h3">Girack</p>
         <br>
-        <p class="text-h5">iikanji</p>
-        <p>ビルド : alpha_20230324_1</p>
+        <p class="text-h5">compact...</p>
+        <p>ビルド : alpha_20230325</p>
         <br>
         <p>それ以外書くことな</p>
     </div>
