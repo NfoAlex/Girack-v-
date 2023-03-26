e606234697a1e55b7312f1f2baa2075c058bffff
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-03-26 19:17:26 +0900

    [change] - ビルド名の変更

--- Girack-v/src/components/MenuComponent/AboutGirack.vue
+++ Girack-v/src/components/MenuComponent/AboutGirack.vue
@@ -5,7 +5,7 @@
     <div class="text-center pa-5" style="width:80%">
         <p class="text-h3">Girack</p>
         <br>
-        <p class="text-h5">onlyfixes</p>
+        <p class="text-h5">Bigger</p>
         <p>ビルド : alpha_20230326</p>
         <br>
         <p>それ以外書くことな</p>
