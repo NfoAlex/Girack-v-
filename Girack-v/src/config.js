b025017ec39d5773f57efa700b4f2509380731f8
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-04-01 00:26:58 +0900

    [add] - 設定項目にコメント追加

--- Girack-v/src/config.js
+++ Girack-v/src/config.js
@@ -6,17 +6,18 @@ import { ref } from "vue";
 //通知設定
 const CONFIG_NOTIFICATION = ref({
     ENABLE: true, //ブラウザ通知
-    NOTIFY_ALL: false,
-    NOTIFY_MENTION: true,
+    NOTIFY_ALL: false, //すべてを通知する
+    NOTIFY_MENTION: true, //メンションを通知する
 
-    DISPLAY_TAB_NEW: false,
-    DISPLAY_TAB_MENTION: true,
+    DISPLAY_TAB_NEW: false, //タブに新着数を出す
+    DISPLAY_TAB_MENTION: true, //タブにメンション数を出す
 });
 
 //通知をミュートするチャンネルリスト
 const LIST_NOTIFICATION_MUTE_CHANNEL = ref([]);
 const LIST_NOTIFICATION_MUTE_USER = ref([]);
 
+//UIに関する設定
 const CONFIG_DISPLAY = ref({
 
 });
