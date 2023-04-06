e6b645b087001ca3ee69e8a7fb997651cb2d8f54
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-04-06 23:14:32 +0900

    [add] - 入力をしながらのメンション用のユーザー検索機能のプロトタイプ追加

--- Girack-v/src/components/ChannelWindowComponent/Input.vue
+++ Girack-v/src/components/ChannelWindowComponent/Input.vue
@@ -13,6 +13,39 @@ export default {
         return {
             txt: "",
             channelid: "",
+            searchMode: {
+                enabled: false, //検索モードに入っているかどうか
+                searchingTerm: "", //検索するもの("user" | "channel")
+                indexStarting: 0 //検索を始めた文字数
+            }
+        }
+    },
+
+    watch: {
+        //入力したテキストを監視して
+        txt() {
+            //@が入力されたら検索モードに入る
+            if ( this.txt[this.txt.length-1] === "@" ) {
+                this.searchMode.enabled = true;
+                this.searchMode.indexStarting = this.txt.length-1;
+
+            }
+
+            //スペースが入力されたら検索モードを終了
+            if ( this.txt[this.txt.length-1] === " " || this.txt[this.txt.length-1] === "　" ) {
+                this.searchMode.enabled = false;
+
+            }
+
+            //検索モードに入っているなら検索する
+            if ( this.searchMode.enabled ) {
+                //検索文字列を取得
+                let searchQuery = this.txt.substring(this.searchMode.indexStarting+1);
+
+                console.log("Input :: watch(txt) : 検索する文字列 -> ", searchQuery);
+
+            }
+
         }
     },
 
@@ -31,6 +64,7 @@ export default {
                 userid: Userinfo.value.userid, //名前
                 channelid: this.getPath, //チャンネルID
                 sessionid: Userinfo.value.sessionid, //セッションID);
+                isReply: false,
                 content: this.txt //メッセージの本文
             });
             
@@ -65,18 +99,35 @@ export default {
     <div>
         <div style="width:90%; height:fit-content;" class="mx-auto d-flex align-center">
             <v-container fill-height fluid class="d-flex">
-                <v-text-field
-                    style="height:fit-content"
-                    ref="inp"
-                    :placeholder="getChannelname() + 'へ送信'"
-                    @keydown.enter="msgSend"
-                    variant="solo"
-                    density="compact"
-                    clearable
-                    v-model="txt"
-                    :single-line="true"
+
+                <v-menu
+                    label="list"
+                    location="top"
                 >
-                </v-text-field>
+                    <template v-slot:activator="{ props }">
+                        <v-text-field
+                            style="height:fit-content"
+                            ref="inp"
+                            :placeholder="getChannelname() + 'へ送信'"
+                            @keydown.enter="msgSend"
+                            variant="solo"
+                            density="compact"
+                            clearable
+                            v-model="txt"
+                            v-bind="props"
+                            :single-line="true"
+                        >
+                        </v-text-field>
+                    </template>
+
+                    <v-list v-if="searchMode.enabled">
+                        <v-list-item
+                            v-for="i in ['alex', 'guest', 'guy']"
+                        >
+                            {{ i }}
+                        </v-list-item>
+                    </v-list>
+                </v-menu>
             
 
 
