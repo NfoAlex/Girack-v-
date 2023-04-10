485b22ac4a19a3bb446e52ab4bb8f70a70d58222
バシュロアレキサンダーレイ <40430040+NfoAlex@users.noreply.github.com>
2023-04-10 12:18:39 +0900

    [add] - ビルド情報の更新

--- Girack-v/src/components/MenuPageComponent/AboutGirack.vue
+++ Girack-v/src/components/MenuPageComponent/AboutGirack.vue
@@ -5,8 +5,8 @@
     <div class="text-center pa-5" style="width:80%">
         <p class="text-h3">Girack</p>
         <br>
-        <p class="text-h5">Enhance</p>
-        <p>ビルド : alpha_20230406</p>
+        <p class="text-h5">Layout2</p>
+        <p>ビルド : alpha_20230410</p>
         <br>
         <p>それ以外書くことな</p>
     </div>
