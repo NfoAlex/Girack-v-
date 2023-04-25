14e3c1d9182114b3a585733ac8193bbdb02f186c
バシュロアレキサンダーレイ <40430040+NfoAlex@users.noreply.github.com>
2023-04-25 12:13:04 +0900

    [add] - チャット欄にてファイル容量の表示に単位をつけるように

--- Girack-v/src/components/ChannelWindowComponent/ContentAttatchmentRender.vue
+++ Girack-v/src/components/ChannelWindowComponent/ContentAttatchmentRender.vue
@@ -11,6 +11,31 @@ export default {
             imageDialogShow: false, //画像拡大ダイアログ用
             imageDialogSrc: ""
         }
+    },
+
+    methods: {
+        //ファイルサイズの値を読める形の単位に変換(https://stackoverflow.com/questions/10420352/converting-file-size-in-bytes-to-human-readable-string)
+        humanFileSize(bytes, si=false, dp=1) {
+            const thresh = si ? 1000 : 1024;
+
+            if (Math.abs(bytes) < thresh) {
+                return bytes + ' B';
+            }
+
+            const units = si 
+                ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'] 
+                : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+            let u = -1;
+            const r = 10**dp;
+
+            do {
+                bytes /= thresh;
+                ++u;
+            } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
+
+
+            return bytes.toFixed(dp) + ' ' + units[u];
+        }
     }
 
 }
@@ -59,7 +84,7 @@ export default {
                 <p class="text-subtitle-1">
                     <a target="_blank" :href="filesrc+file.fileid">{{ file.name }}</a>
                 </p>
-                <p class="text-medium-emphasis">サイズ: <v-chip size="small">{{ file.size }}</v-chip> | 種類: <v-chip size="small">{{ file.type }}</v-chip></p>
+                <p class="text-medium-emphasis">サイズ: <v-chip size="small">{{ humanFileSize(file.size) }}</v-chip> | 種類: <v-chip size="small">{{ file.type }}</v-chip></p>
             </span>
 
         </v-card>
