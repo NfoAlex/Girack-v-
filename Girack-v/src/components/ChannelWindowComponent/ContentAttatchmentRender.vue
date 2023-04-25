8209f5039cae38ed420b8c1de534eb3a868c324a
バシュロアレキサンダーレイ <40430040+NfoAlex@users.noreply.github.com>
2023-04-25 10:44:21 +0900

    [add] - 画像ファイルの添付なら画像を直接表示

--- Girack-v/src/components/ChannelWindowComponent/ContentAttatchmentRender.vue
+++ Girack-v/src/components/ChannelWindowComponent/ContentAttatchmentRender.vue
@@ -18,12 +18,17 @@ export default {
 <template>
     <div>
         <v-card
-            class="rounded-lg pa-2 ma-2"
+            class="rounded-lg pa-3 ma-2 d-flex justify-space-between"
             style="max-width:50%; width:fit-content"
             v-for="file in fileData.attatchmentData"
         >
-            <p><a target="_blank" :href="filesrc+file.fileid">{{ file.name }}</a></p>
-            <p>サイズ: <v-chip size="small">{{ file.size }}</v-chip> | 種類: <v-chip size="small">{{ file.type }}</v-chip></p>
+            <span v-if="file.type.includes('image/')">
+                <img style="max-height: 150px;" :src="filesrc+file.fileid">
+            </span>
+            <span style="margin-left:16px;">
+                <p><a target="_blank" :href="filesrc+file.fileid">{{ file.name }}</a></p>
+                <p>サイズ: <v-chip size="small">{{ file.size }}</v-chip> | 種類: <v-chip size="small">{{ file.type }}</v-chip></p>
+            </span>
         </v-card>
     </div>
 </template>
