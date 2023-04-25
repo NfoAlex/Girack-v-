6fbfcc7be5fdd8ba4b098152faf9a85c72e9a386
バシュロアレキサンダーレイ <40430040+NfoAlex@users.noreply.github.com>
2023-04-25 11:10:30 +0900

    [add] - 添付ファイルの画像を拡大できるように

--- Girack-v/src/components/ChannelWindowComponent/ContentAttatchmentRender.vue
+++ Girack-v/src/components/ChannelWindowComponent/ContentAttatchmentRender.vue
@@ -7,7 +7,9 @@ export default {
 
     data() {
         return {
-            filesrc: backendURI + "/file/" + this.channelid + "/"
+            filesrc: backendURI + "/file/" + this.channelid + "/",
+            imageDialogShow: false, //画像拡大ダイアログ用
+            imageDialogSrc: ""
         }
     }
 
@@ -16,19 +18,49 @@ export default {
 </script>
 
 <template>
+
+    <!-- 画像拡大ダイアログ -->
+    <v-dialog
+        v-model="imageDialogShow"
+        style="max-width:90vw;"
+    >
+        <div style="overflow-y:auto;">
+            <div @click="imageDialogShow=false" class="mx-auto" style="width:95%;">
+
+                <v-card
+                    style="width:fit-content; margin:64px 0;"
+                    color="rgba(0,0,0,0.75)"
+                    class="rounded-lg mx-auto"
+                >
+                    <!-- 画像そのもの -->
+                    <v-img style="margin-bottom:8px; max-height:90vh; cursor:pointer;" :src="imageDialogSrc">
+                    </v-img>
+                    <!-- 画像URL -->
+                    <p class="ma-2 text-subtitle-2">{{ imageDialogSrc }}</p>
+                </v-card>
+
+            </div>
+        </div>
+    </v-dialog>
+
     <div>
         <v-card
             class="rounded-lg pa-3 ma-2 d-flex justify-space-between"
             style="max-width:50%; width:fit-content"
             v-for="file in fileData.attatchmentData"
         >
+
             <span v-if="file.type.includes('image/')">
-                <img style="max-height: 150px;" :src="filesrc+file.fileid">
+                <img @click="imageDialogShow=true;imageDialogSrc=filesrc+file.fileid;" style="max-height: 150px; cursor:pointer;" :src="filesrc+file.fileid">
             </span>
+
             <span style="margin-left:16px;">
-                <p><a target="_blank" :href="filesrc+file.fileid">{{ file.name }}</a></p>
+                <p class="text-subtitle-1">
+                    <a target="_blank" :href="filesrc+file.fileid">{{ file.name }}</a>
+                </p>
                 <p>サイズ: <v-chip size="small">{{ file.size }}</v-chip> | 種類: <v-chip size="small">{{ file.type }}</v-chip></p>
             </span>
+            
         </v-card>
     </div>
 </template>
