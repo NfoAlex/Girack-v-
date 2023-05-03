899b7a9a3a717db22f9eeb8f1c6693e60e915806
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-05-03 23:50:31 +0900

    [add] - ツイートじゃないTwitterのリンクの場合のボタンを追加

--- Girack-v/src/components/ChannelWindowComponent/ContentURLpreview.vue
+++ Girack-v/src/components/ChannelWindowComponent/ContentURLpreview.vue
@@ -91,6 +91,17 @@ export default {
                 <span v-else>埋め込みを隠す<v-icon>mdi:mdi-window-close</v-icon></span>
             </v-btn>
         </div>
+        
+        <!-- ツイートじゃないTwitterのリンクの場合 -->
+        <div class="pa-3" v-if="link.url.includes('https://twitter.com/')&&!link.url.includes('/status/')">
+            <a :href="link.url" target="_blank">
+                <v-btn class="rounded-lg" color="blue" size="small">
+                    <v-icon>mdi:mdi-open-in-new</v-icon>
+                    Twitterのページへ飛ぶ
+                </v-btn>
+            </a>
+        </div>
+
         <!-- Twitter埋め込み表示 -->
         <div v-if="embedTwitter">
             <Tweet
