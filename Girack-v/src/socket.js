8a8639ebcdc36e40c3f941eac4400c88b4e94005
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-05-22 22:35:22 +0900

    [change] - ビルド情報の更新

--- Girack-v/src/socket.js
+++ Girack-v/src/socket.js
@@ -6,7 +6,7 @@ import { ref } from "vue";
 
 import { getCONFIG } from './config.js';
 
-export const CLIENT_VERSION = "alpha_20230521";
+export const CLIENT_VERSION = "alpha_20230522";
 
 const {
     CONFIG_SYNC,
