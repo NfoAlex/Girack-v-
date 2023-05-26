dcaa05f5c534725ab7664f5fe872188f31f460e1
バシュロアレキサンダーレイ <vashro.alex1@protonmail.com>
2023-05-26 10:51:30 +0900

    [change] - バージョン情報の更新

--- Girack-v/src/socket.js
+++ Girack-v/src/socket.js
@@ -6,7 +6,7 @@ import { ref } from "vue";
 
 import { getCONFIG } from './config.js';
 
-export const CLIENT_VERSION = "alpha_20230523";
+export const CLIENT_VERSION = "alpha_20230526";
 
 const {
     CONFIG_SYNC,
