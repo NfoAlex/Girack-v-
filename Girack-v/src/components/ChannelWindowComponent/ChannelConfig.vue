70289bcbc305903fa91270d02792712ebe7e0334
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-06-04 19:26:11 +0900

    [fix] - チャンネル設定にもSocketハンドラ割り当てを適用

--- Girack-v/src/components/ChannelWindowComponent/ChannelConfig.vue
+++ Girack-v/src/components/ChannelWindowComponent/ChannelConfig.vue
@@ -176,6 +176,26 @@ export default {
                 }
             });
 
+        },
+
+        SOCKETinfoChannelJoinedUserList(channelJoinedUserList) {
+            //ユーザー名でソートして追加
+            this.channelJoinedUser = channelJoinedUserList.sort((u1, u2) => {
+                if ( u1.username > u2.username ) {
+                    return 1;
+
+                } else {
+                    return -1;
+
+                }
+
+            });
+
+        },
+
+        SOCKETinfoSearchUser(result) {
+            this.userSearchResult = result;
+
         }
 
     },
@@ -189,26 +209,10 @@ export default {
 
 
         //チャンネル参加者リストを受信
-        socket.on("infoChannelJoinedUserList", (channelJoinedUserList) => {
-            //ユーザー名でソートして追加
-            this.channelJoinedUser = channelJoinedUserList.sort((u1, u2) => {
-                if ( u1.username > u2.username ) {
-                    return 1;
-
-                } else {
-                    return -1;
-
-                }
-
-            });
-
-        });
+        socket.on("infoChannelJoinedUserList", this.SOCKETinfoChannelJoinedUserList);
 
         //ユーザー名検索の結果受け取り用
-        socket.on("infoSearchUser", (result) => {
-            this.userSearchResult = result;
-
-        });
+        socket.on("infoSearchUser", this.SOCKETinfoSearchUser);
 
         //チャンネルに参加しているユーザーリストを取得
         socket.emit("getInfoChannelJoinedUserList", {
@@ -223,8 +227,8 @@ export default {
 
     unmounted() {
         //通信重複防止
-        socket.off("infoChannelJoinedUserList");
-        socket.off("infoSearchUser");
+        socket.off("infoChannelJoinedUserList", this.SOCKETinfoChannelJoinedUserList);
+        socket.off("infoSearchUser", this.SOCKETinfoSearchUser);
 
     }
 
