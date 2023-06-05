d18698179c920c317b235ffcec5f085ef8d8bcca
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-06-05 22:41:02 +0900

    [change] - 初期のユーザーデータホルダーを変更

--- Girack-v/src/data/dataUserinfo.js
+++ Girack-v/src/data/dataUserinfo.js
@@ -2,7 +2,7 @@ import { ref } from "vue";
 
 const myUserinfo = ref({
     username: "User", //名前
-    role: "Admin",
+    role: "Member",
     userid: "001", //ユーザーID
     loggedin: false, //ログイン状態
     sessionid: 0, //セッションID
