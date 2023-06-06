d46f13b68cb8a5a209507bf270f62cf5840f9fa8
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-06-06 14:06:37 +0900

    [change] - 余分な処理をしていた

--- Girack-v/src/components/ChannelWindowComponent/Content.vue
+++ Girack-v/src/components/ChannelWindowComponent/Content.vue
@@ -131,8 +131,8 @@ export default {
 
             //レンダーを待ってからスクロール
             this.$nextTick(() => {
-                //チャンネル以外のページ場合、あるいは別のチャンネルでの処理が続いている場合、以降の処理をスキップする
-                if ( !(newPage.path.startsWith('/c/')) || this.getPath !== newPage.params.id ) {
+                //チャンネル以外のページ場合、これ以降の処理をスキップする
+                if ( !(newPage.path.startsWith('/c/')) ) {
                     console.log("Content :: watch($route) : スクロールしないわ", this.channelInfo.channelid, newPage.params.id);
                     return 0;
 
