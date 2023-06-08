342fcbcf3f17b30f1ae931d524a029e1884d3cb1
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-06-08 19:28:23 +0900

    [change]  - 監査ログの土台を整備

--- Girack-v/src/components/MenuPageComponent/Modlog.vue
+++ Girack-v/src/components/MenuPageComponent/Modlog.vue
@@ -1,8 +1,18 @@
 <script>
+import { getSocket } from '../../data/socket';
+const socket = getSocket();
+
+export default {
+
+}
 </script>
 
 <template>
-    <div style="width:85%">
-        <p>監査ー</p>
+    <div style="height:100vh; width:90%;" class="d-flex align-center flex-column">
+        <div style="width:90%; padding-top:3%" class="text-left align-center">
+            <p class="text-left" style="font-size:min(4vh,36px)">
+                監査ログ
+            </p>
+        </div>
     </div>
 </template>
