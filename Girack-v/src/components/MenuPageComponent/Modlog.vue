5c3f75e9fefd9718bef1419d3e9a37c13064d50b
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-06-08 19:35:02 +0900

    [add] - 監査ログを受け取る土台を準備

--- Girack-v/src/components/MenuPageComponent/Modlog.vue
+++ Girack-v/src/components/MenuPageComponent/Modlog.vue
@@ -3,16 +3,32 @@ import { getSocket } from '../../data/socket';
 const socket = getSocket();
 
 export default {
+    data() {
+        return {
+            modlog: []
+        }
+    },
+
+    methods: {
+        SOCKETinfoModlog() {
+
+        }
+    }
 
 }
 </script>
 
 <template>
     <div style="height:100vh; width:90%;" class="d-flex align-center flex-column">
+        <!-- ページタイトル -->
         <div style="width:90%; padding-top:3%" class="text-left align-center">
             <p class="text-left" style="font-size:min(4vh,36px)">
                 監査ログ
             </p>
+        </div>
+
+        <div>
+
         </div>
     </div>
 </template>
