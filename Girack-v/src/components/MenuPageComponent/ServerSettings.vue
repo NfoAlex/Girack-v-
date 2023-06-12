6339a7d36d9732bce17f68ad23a2f8c8f25f42bd
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-06-12 14:36:53 +0900

    [add] - レンダーのディレイ、これも必要

--- Girack-v/src/components/MenuPageComponent/ServerSettings.vue
+++ Girack-v/src/components/MenuPageComponent/ServerSettings.vue
@@ -14,6 +14,7 @@ export default {
                 inviteOnly: false,
                 config: {}
             },
+            configReady: false,
 
             displayServername: "...",
             displaySettings: {
