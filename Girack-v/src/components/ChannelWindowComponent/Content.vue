a22ab0cb76211d416d18d321c74496e792f97342
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-06-26 21:46:12 +0900

    [fix] - 表示メッセージが過去にいっちゃっても新着線を残すように

--- Girack-v/src/components/ChannelWindowComponent/Content.vue
+++ Girack-v/src/components/ChannelWindowComponent/Content.vue
@@ -751,11 +751,11 @@ export default {
 
             <!-- 新着メッセージ線 -->
             <span
-                v-if="m.time===MsgReadTimeBefore&&index!==msgDisplayNum-1"
+                v-if="m.time===MsgReadTimeBefore||(index===0&&m.time>MsgReadTimeBefore)&&MsgReadTimeBefore!==''"
                 class="d-flex align-center"
             >
                 <v-divider color="white" thickness="2px" class="flex-shrink-1 flex-grow-0"></v-divider>
-                <v-chip style="margin:-1em;" variant="flat" elevation="6" class="pa-2 flex-grow-1 flex-shrink-0" size="x-small">ここから新着({{ index }} : {{ m.time }})</v-chip>
+                <v-chip style="margin:-1em;" variant="flat" elevation="6" class="pa-2 flex-grow-1 flex-shrink-0" size="x-small">ここから新着</v-chip>
                 <v-divider color="white" thickness="2px" class="flex-shrink-1 flex-grow-0"></v-divider>
             </span>
 
