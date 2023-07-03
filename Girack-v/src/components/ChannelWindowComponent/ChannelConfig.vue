a312e98646d6278fb6817ace36a070d10f3fa8a3
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-07-03 14:17:42 +0900

    [add] - 検索クエリーが空でもユーザー検索をするように

--- Girack-v/src/components/ChannelWindowComponent/ChannelConfig.vue
+++ Girack-v/src/components/ChannelWindowComponent/ChannelConfig.vue
@@ -51,20 +51,14 @@ export default {
       handler() {
         //もし検索画面を開いてるなら
         if (this.userSearchShow) {
-          //検索文字列が１文字以上なら
-          if (this.userSearchQuery.length >= 1) {
-            //検索クエリーを送信
-            socket.emit("searchUserDynamic", {
-              query: this.userSearchQuery,
-              reqSender: {
-                userid: this.myUserinfo.userid,
-                sessionid: this.myUserinfo.sessionid,
-              },
-            });
-          } else {
-            //もし文字列が０文字なら
-            this.userSearchResult = []; //検索結果を初期化
-          }
+          //検索クエリーを送信
+          socket.emit("searchUserDynamic", {
+            query: this.userSearchQuery,
+            reqSender: {
+              userid: this.myUserinfo.userid,
+              sessionid: this.myUserinfo.sessionid,
+            },
+          });
         }
       },
     },
