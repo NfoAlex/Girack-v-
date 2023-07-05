9428c6394b4891d5b078d540f85c22712fb3ee7c
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-07-05 13:18:09 +0900

    [add] - セッション管理をするページコンポーネントを追加

---
+++ Girack-v/src/components/MenuPageComponent/SessionManage.vue
@@ -0,0 +1,24 @@
+<script>
+
+</script>
+
+<template>
+  <div style="width: 90%;">
+    <div style="padding-top: 3%; margin-bottom: 16px">
+      <p class="text-truncate" style="font-size: min(4vh, 36px)">
+      セッション管理
+      </p>
+    </div>
+    <div>
+      <v-expansion-panels style="width: 90%">
+        <v-expansion-panel
+          class="rounded-lg"
+        >
+          <v-expansion-panel-title>
+            この日のログイン
+          </v-expansion-panel-title>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </div>
+  </div>
+</template>
