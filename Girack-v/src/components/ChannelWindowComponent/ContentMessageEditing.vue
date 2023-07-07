d3bd18994d35b3fdbd6feefbaf0b6db30ad08c98
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-07-07 09:34:29 +0900

    [add] - 編集中にescキーでキャンセルできるように

--- Girack-v/src/components/ChannelWindowComponent/ContentMessageEditing.vue
+++ Girack-v/src/components/ChannelWindowComponent/ContentMessageEditing.vue
@@ -48,6 +48,7 @@ export default {
 <template>
   <v-textarea
     @keydown.enter="updateMessage"
+    @keydown.esc="$emit('updateEditingMessage','waaaa')"
     v-model="editTxt"
     variant="outlined"
   >
