fc3f136d717a2702bb9be01a53d4612ff619b704
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-07-08 15:44:22 +0900

    [change] - キーの判定処理を全体的に改善

--- Girack-v/src/components/ChannelWindowComponent/ContentMessageEditing.vue
+++ Girack-v/src/components/ChannelWindowComponent/ContentMessageEditing.vue
@@ -21,10 +21,12 @@ export default {
   },
 
   methods: {
-    //Enterキーのトリガー処理
+    //Enterキーのトリガー処理(keyDownから来てるのでどのキーでも反応はする)
     enterTrigger(event) {
       //変換中のEnterなら処理させない
       if (event.keyCode !== 13) return;
+      //削除確認中だったらメッセージの消去を行う
+      if (this.dialogCheckToDelete) this.deleteMessage();
 
       //もしShiftキーも押されていたら
       if (event.shiftKey) {
@@ -57,8 +59,24 @@ export default {
       this.updateMessage();
     },
 
+    //escキーのトリガー処理(keyDownから来てるのでどのキーでも反応はする)
+    escTrigger(event) {
+      //そもそも押されたのがescキーじゃないなら停止
+      if (event.key !== "Escape") return;
+      
+      //削除確認中かどうかで処理を変える
+      if (this.dialogCheckToDelete) { //確認中なら
+        //ダイアログを非表示に
+        this.dialogCheckToDelete = false;
+      } else { //確認中じゃないなら
+        //メッセージ編集を止める
+        this.$emit('updateEditingMessage','waaaa')
+      }
+    },
+
     //メッセージの編集を適用する関数
     updateMessage() {
+      console.log("更新しようとしてる");
       //更新するように送信
       socket.emit("editMessage", {
         textEditing: this.editTxt,
@@ -94,6 +112,10 @@ export default {
     //propsからメッセージ分を取得、格納
     this.editTxt = this.content;
 
+    //キーの監視開始
+    document.addEventListener("keydown", this.enterTrigger);
+    document.addEventListener("keydown", this.escTrigger);
+
     //カーソルを最後に持ってくる
     this.$nextTick(() => {
       let DOMeditingTextArea = document.getElementById("editingTextArea");
@@ -108,6 +130,12 @@ export default {
         DOMeditingTextArea.setSelectionRange(this.editTxt.length, this.editTxt.length);
       }, 10);
     });
+  },
+
+  unmounted() {
+    //キーの監視停止
+    document.removeEventListener("keydown", this.enterTrigger);
+    document.removeEventListener("keydown", this.escTrigger);
   }
 }
 
@@ -115,70 +143,72 @@ export default {
 
 <template>
   <div>
-  <!-- メッセージの削除確認 -->
-  <v-dialog
-    v-model="dialogCheckToDelete"
-    @keydown.enter="deleteMessage"
-    width="40vh"
-  >
-    <v-card class="rounded-lg pa-5">
-      <v-card-title>
-        消去の確認
-      </v-card-title>
-      <p>
-        消していいの
-      </p>
-      <span class="d-flex justify-end pa-1">
-        <v-btn class="ma-1 rounded-lg" color="cardInner">
-          だめ
-        </v-btn>
-        <v-btn @click="deleteMessage" class="ma-1 rounded-lg" color="success">
-          いいよ
-        </v-btn>
-      </span>
-    </v-card>
-  </v-dialog>
-
-  <!-- 編集部分 -->
-  <v-textarea
-    id="editingTextArea"
-    @keydown.enter.prevent="enterTrigger"
-    @keydown.esc="$emit('updateEditingMessage','waaaa')"
-    v-model="editTxt"
-    variant="outlined"
-  >
-    <template v-slot:append-inner>
-      <v-btn
-        @click="updateMessage"
-        :disabled="Serverinfo.config.MESSAGE.MESSAGE_TXT_MAXLENGTH<editTxt.length"
-        class="rounded-lg ma-1"
-        size="x-small"
-        icon=""
-        color="secondary"
-      >
-        <v-icon
-          v-if="Serverinfo.config.MESSAGE.MESSAGE_TXT_MAXLENGTH>=editTxt.length"
+    <!-- 編集部分 -->
+    <v-textarea
+      id="editingTextArea"
+      @keydown.enter.prevent="null"
+      v-model="editTxt"
+      variant="outlined"
+    >
+      <template v-slot:append-inner>
+        <v-btn
+          @click="updateMessage"
+          :disabled="Serverinfo.config.MESSAGE.MESSAGE_TXT_MAXLENGTH<editTxt.length"
+          class="rounded-lg ma-1"
+          size="x-small"
+          icon=""
+          color="secondary"
         >
-          mdi:mdi-check-bold
-        </v-icon>
-        <span v-else>
-          -
-          {{ 
-            editTxt.length
+          <v-icon
+            v-if="Serverinfo.config.MESSAGE.MESSAGE_TXT_MAXLENGTH>=editTxt.length"
+          >
+            mdi:mdi-check-bold
+          </v-icon>
+          <span v-else>
             -
-            Serverinfo.config.MESSAGE.MESSAGE_TXT_MAXLENGTH
-          }}
+            {{ 
+              editTxt.length
+              -
+              Serverinfo.config.MESSAGE.MESSAGE_TXT_MAXLENGTH
+            }}
+          </span>
+        </v-btn>
+        <v-btn
+          @click="$emit('updateEditingMessage','waaaa')"
+          class="rounded-lg ma-1"
+          size="x-small"
+          icon="mdi:mdi-window-close"
+          color="secondary"
+        >
+        </v-btn>
+      </template>
+    </v-textarea>
+
+    <v-dialog
+      v-model="dialogCheckToDelete"
+      class="ma-3 mx-auto"
+      width=""
+      contained
+    >
+      <v-card class="rounded-lg pa-5">
+        <v-card-title>
+          消去の確認
+        </v-card-title>
+        <v-card-item>
+          消していいの
+        </v-card-item>
+        <span class="d-flex justify-end align-center pa-1">
+          <span class="text-disabled me-auto text-body-2">
+            Enterキーでも選択できます
+          </span>
+          <v-btn @click="dialogCheckToDelete=false" class="ma-1 rounded-lg" color="cardInner">
+            だめ
+          </v-btn>
+          <v-btn @click="deleteMessage" class="ma-1 rounded-lg" color="success">
+            いいよ
+          </v-btn>
         </span>
-      </v-btn>
-      <v-btn
-        @click="$emit('updateEditingMessage','waaaa')"
-        class="rounded-lg ma-1"
-        size="x-small"
-        icon="mdi:mdi-window-close"
-        color="secondary"
-      >
-      </v-btn>
-    </template>
-  </v-textarea>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
