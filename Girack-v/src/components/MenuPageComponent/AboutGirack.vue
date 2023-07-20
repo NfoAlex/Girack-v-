7be94442d75640caef958c5234cbcbf37501202a
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-07-20 18:04:57 +0900

    [change] - 可読性

--- Girack-v/src/components/MenuPageComponent/AboutGirack.vue
+++ Girack-v/src/components/MenuPageComponent/AboutGirack.vue
@@ -4,23 +4,23 @@ import { CLIENT_VERSION } from "../../data/socket.js";
 
 export default {
 
-    data() {
-        return {
-            clientVersion: CLIENT_VERSION //バージョン名
-        }
+  data() {
+    return {
+        clientVersion: CLIENT_VERSION //バージョン名
     }
+  }
 
 }
 
 </script>
 
 <template>
-    <div class="text-center pa-5" style="width:80%">
-        <p class="text-h3">JustScroll</p>
-        <br>
-        <p class="text-h5"></p>
-        <p>ビルド : {{ clientVersion }}</p>
-        <br>
-        <p>それ以外書くことな</p>
-    </div>
+  <div class="text-center pa-5" style="width:80%">
+    <p class="text-h3">JustScroll</p>
+    <br>
+    <p class="text-h5"></p>
+    <p>ビルド : {{ clientVersion }}</p>
+    <br>
+    <p>それ以外書くことな</p>
+  </div>
 </template>
