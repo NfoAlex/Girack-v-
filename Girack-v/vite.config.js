a0bcf72ad1121b197fd7e168847ac9a8e6fb0a73
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-07-20 17:15:57 +0900

    [add] - PWAホストをするための設定を追加

--- Girack-v/vite.config.js
+++ Girack-v/vite.config.js
@@ -2,10 +2,18 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      injectRegister: 'auto',
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
