7024f068b6463b45b858a17e758d40c30a5691a6
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-10-11 23:28:41 +0900

    [change] - 全既読ボタンの設定項目の場所変更

--- Girack-v/src/components/MenuPageComponent/Settings.vue
+++ Girack-v/src/components/MenuPageComponent/Settings.vue
@@ -404,6 +404,18 @@ export default {
                 density="compact"
               >
               </v-checkbox>
+            </v-card>
+
+            <br />
+
+            <p><v-icon>mdi:mdi-format-list-group</v-icon>サイドバー</p>
+            <v-card color="cardInner" class="cardInner pa-3 rounded-lg">
+              <p class="pa-1">チャンネルの表示順番</p>
+              <v-select
+                v-model="CONFIG_DISPLAY.SIDEBAR_CHANNEL_ORDERBY"
+                :items="['alphabetical', 'id']"
+              >
+              </v-select>
               <v-checkbox
                 v-model="CONFIG_DISPLAY.SIDEBAR_SHOWREADALL_ENABLED"
                 label="全チャンネル既読ボタンを有効化"
@@ -418,18 +430,6 @@ export default {
                 density="compact"
               >
               </v-checkbox>
-            </v-card>
-
-            <br />
-
-            <p><v-icon>mdi:mdi-format-list-group</v-icon>サイドバー</p>
-            <v-card color="cardInner" class="cardInner pa-3 rounded-lg">
-              <p class="pa-1">チャンネルの表示順番</p>
-              <v-select
-                v-model="CONFIG_DISPLAY.SIDEBAR_CHANNEL_ORDERBY"
-                :items="['alphabetical', 'id']"
-              >
-              </v-select>
             </v-card>
           </v-card>
 
