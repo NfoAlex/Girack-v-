7e7c36017e38c2cd6c03bf9fd3a97566fc7f4970
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-12-03 14:04:13 +0900

    [add] - 返信レンダー用コンポでのprop受け取り

--- Girack-v/src/components/ChannelWindowComponent/ContentComponents/contentReplyRender.vue
+++ Girack-v/src/components/ChannelWindowComponent/ContentComponents/contentReplyRender.vue
@@ -3,13 +3,17 @@
 import { getSocket } from "../../../data/socket.js";
 import { dataUser } from "../../../data/dataUserinfo";
 
+const socket = getSocket();
+
 export default {
 
   setup() {
     const { UserIndex } = dataUser();
 
     return { UserIndex };
-  }
+  },
+
+  props: ["messageid", "channelid"],
 
 }
 
