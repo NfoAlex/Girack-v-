37746286a0bbbe99976e8e564c1678ef84b076d4
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-12-03 16:13:28 +0900

    [add] - ピン留めを表示するコンポ作成

---
+++ Girack-v/src/components/ChannelWindowComponent/HeadComponents/ChannelPin.vue
@@ -0,0 +1,30 @@
+<script>
+
+import { getSocket, Serverinfo } from "../../../data/socket";
+import { dataUser } from "../../../data/dataUserinfo";
+
+const socket = getSocket();
+
+export default {
+
+    setup() {
+        const { myUserinfo } = dataUser();
+        return { myUserinfo };
+    },
+
+    props: ["pins"]
+
+}
+
+</script>
+
+<template>
+    <div>
+        <v-dialog>
+            <v-card>
+                asdf
+                {{ pins }}
+            </v-card>
+        </v-dialog>
+    </div>
+</template>
