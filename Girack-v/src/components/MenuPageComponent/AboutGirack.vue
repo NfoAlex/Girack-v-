f7e843f1460c7e6f11685846d8bff5d45359111c
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-12-12 18:07:25 +0900

    [change] - バージョン情報の更新

--- Girack-v/src/components/MenuPageComponent/AboutGirack.vue
+++ Girack-v/src/components/MenuPageComponent/AboutGirack.vue
@@ -17,7 +17,7 @@ export default {
 <template>
   <div class="text-center pa-5" style="width:80%">
     <span>
-      <p class="text-h3">SystemMessageIcons</p>
+      <p class="text-h3">SelectTheUser</p>
     </span>
     
     <br>
