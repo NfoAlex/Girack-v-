d55ba76fa21c489d9f17b1b1dfd902f7aacaa789
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-12-18 19:16:10 +0900

    [add] - 土台となる要素追加

--- Girack-v/src/components/MenuPageComponent/APIConfigure.vue
+++ Girack-v/src/components/MenuPageComponent/APIConfigure.vue
@@ -3,11 +3,13 @@
 export default {
 
   data() {
-
+    return {
+      displayDialogRegister: false,
+    }
   },
 
   mounted() {
-    console.log("API...");
+    console.log("API...?");
   }
 
 }
@@ -16,12 +18,27 @@ export default {
 
 <template>
   <div>
-    <div class="mx-auto d-flex flex-column" style="width: 90%; height:100vh;">
-      <div class="d-flex align-center" style="padding-top: 3%; margin-bottom: 16px">
+
+    <v-dialog v-model="displayDialogRegister">
+      <v-card>
+        asdf
+      </v-card>
+    </v-dialog>
+
+    <div class="mx-auto d-flex flex-column" style="width:90%;height:100vh;">
+      <div class="d-flex align-center my-3">
         <p class="text-truncate me-auto" style="font-size: min(4vh, 36px)">
         API管理
         </p>
+        <v-btn
+          color="primary"
+          size="large"
+          class="rounded-lg"
+          icon="mdi:mdi-plus"
+        >
+        </v-btn>
       </div>
     </div>
+
   </div>
 </template>
