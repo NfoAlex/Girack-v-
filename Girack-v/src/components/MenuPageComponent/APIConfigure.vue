0ebc7fe1e8c7b798623294490be6caf8927cc9e4
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-12-18 23:49:59 +0900

    [add] - テンプレート表示の土台作り

--- Girack-v/src/components/MenuPageComponent/APIConfigure.vue
+++ Girack-v/src/components/MenuPageComponent/APIConfigure.vue
@@ -56,9 +56,63 @@ export default {
 <template>
   <div>
 
-    <v-dialog v-model="displayDialogRegister">
-      <v-card>
-        asdf
+    <!-- API申請用 -->
+    <v-dialog v-model="displayDialogRegister" style="max-width:650px; width:55vw; height:80vh;">
+      <v-card class="rounded-lg pa-4 mx-0" height="100%" width="100%">
+        
+        <v-card-title>
+          APIの登録申請
+        </v-card-title>
+
+        <v-card-text class="my-5" style="overflow-y:auto">
+          <h4>登録名</h4>
+          <v-card class="pa-3 mt-2 mb-5 rounded-lg" variant="tonal">
+            asdf
+          </v-card>
+
+          <h4>APIタイプ</h4>
+          <v-card class="pa-3 mt-2 mb-5 rounded-lg" variant="tonal">
+            Bot or human?
+          </v-card>
+
+          <h4>サーバーのアクセス</h4>
+          <v-card class="pa-3 mt-2 mb-5 rounded-lg" variant="tonal">
+            <p>asdf</p>
+            <p>asdf</p>
+            <p>asdf</p>
+            <p>asdf</p>
+            <p>asdf</p>
+            <p>asdf</p>
+            <p>asdf</p>
+            <p>asdf</p>
+            <p>asdf</p>
+            <p>asdf</p>
+            <p>asdf</p>
+            <p>asdf</p>
+            <p>asdf</p>
+            <p>asdf</p>
+            <p>asdf</p>
+            <p>asdf</p>
+            <p>asdf</p>
+          </v-card>
+        </v-card-text>
+
+        <v-card-action class="d-flex flex-row-reverse">
+          <v-btn
+            @click="() => { displayDialogRegister=false; }"
+            variant="text"
+            class="rounded-lg"
+          >
+            キャンセル
+          </v-btn>
+          <v-btn
+            class="rounded-lg"
+            color="primary"
+          >
+            登録する
+          </v-btn>
+        </v-card-action>
+
       </v-card>
     </v-dialog>
 
@@ -67,7 +121,18 @@ export default {
         <p class="text-truncate me-auto" style="font-size: min(4vh, 36px)">
         API管理
         </p>
+        <!-- APIリスト再取得 -->
         <v-btn
+          @click="fetchApiList"
+          variant="text"
+          size="large"
+          class="rounded-lg mr-2"
+          icon="mdi:mdi-refresh"
+        >
+        </v-btn>
+        <!-- 申請ボタン -->
+        <v-btn
+          @click="() => { displayDialogRegister=true; }"
           color="primary"
           size="large"
           class="rounded-lg"
@@ -75,6 +140,12 @@ export default {
         >
         </v-btn>
       </div>
+
+      <!-- メインフレーム -->
+      <div>
+        {{ Serverinfo.config.API }}
+      </div>
+
     </div>
 
   </div>
