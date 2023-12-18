2c9b0849e81b63979377111c3e44186019aaeb31
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2023-12-18 23:49:38 +0900

    [add] - マウント時にAPIリストを取得するように

--- Girack-v/src/components/MenuPageComponent/APIConfigure.vue
+++ Girack-v/src/components/MenuPageComponent/APIConfigure.vue
@@ -1,15 +1,52 @@
 <script>
 
+import { Serverinfo, getSocket } from '../../data/socket.js';
+import { dataUser } from '../../data/dataUserinfo.js';
+
+const socket = getSocket();
+
 export default {
+  setup() {
+    const { myUserinfo } = dataUser();
+    return { Serverinfo, myUserinfo };
+  },
 
   data() {
     return {
       displayDialogRegister: false,
+
+      registerApiName: "",
+      registerApiActionOnServer: {
+        USER_GETINFO: false,
+        SERVER_GETCONFIG: false,
+        CHANNEL_GETINFO: false,
+        CHANNEL_GETLIST: false
+      }
+    }
+  },
+
+  methods: {
+
+    //APIリストの取得
+    fetchApiList() {
+      socket.emit("getApiList", {
+        reqSender: {
+          userid: this.myUserinfo.userid,
+          sessionid: this.myUserinfo.sessionid
+        }
+      });
+    },
+
+    SOCKETInfoApiList(dat) {
+      console.log("APIConfigure :: SOCKETInfoApiList : dat->", dat);
     }
   },
 
   mounted() {
-    console.log("API...?");
+    //APIリストハンドラ作成
+    socket.on("InfoApiList", this.SOCKETInfoApiList);
+    //APIリストリクエスト
+    this.fetchApiList();
   }
 
 }
