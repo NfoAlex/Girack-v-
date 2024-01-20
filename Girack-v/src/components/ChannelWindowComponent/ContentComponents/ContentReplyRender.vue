6a9297e2126abb78a5bd7884df73a30f69581e90
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-01-21 02:15:38 +0900

    [remove] - 不要

--- Girack-v/src/components/ChannelWindowComponent/ContentComponents/ContentReplyRender.vue
+++ Girack-v/src/components/ChannelWindowComponent/ContentComponents/ContentReplyRender.vue
@@ -70,7 +70,7 @@ export default {
 
   <div>
 
-    <p class="">
+    <p>
       <a :href="'#' + msgReply.messageid">
         <!-- 返信アイコン -->
         <v-icon>mdi:mdi-reply</v-icon>
@@ -95,9 +95,7 @@ export default {
         :content="contentDisplay"
       />
     </p>
-
     
-
   </div>
 
 </template>
