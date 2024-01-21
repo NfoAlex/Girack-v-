095e0ec2190eb08228e2f73afa0f7dbc7280f723
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-01-21 17:01:55 +0900

    [add] - 新規用ヘッダ追加

---
+++ Girack-v/src/components/Header.vue
@@ -0,0 +1,252 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script>
+import { useDisplay } from "vuetify";
+import { getCONFIG } from "../config.js";
+import { setCookie } from "../data/socket";
+import { dataUser } from "../data/dataUserinfo";
+import { dataChannel } from "../data/dataChannel";
+import ChannelConfig from "./ChannelWindowComponent/HeadComponents/ChannelConfig.vue";
+import ChannelPin from "./ChannelWindowComponent/HeadComponents/ChannelPin.vue";
+
+export default {
+  setup() {
+    const { mobile } = useDisplay();
+    const { myUserinfo } = dataUser();
+    const { ChannelIndex } = dataChannel();
+    const { LIST_NOTIFICATION_MUTE_CHANNEL } = getCONFIG();
+    return { mobile, myUserinfo, ChannelIndex, LIST_NOTIFICATION_MUTE_CHANNEL };
+  },
+
+  components: { ChannelConfig, ChannelPin },
+
+  data() {
+    return {
+      //チャンネルメニューダイアログ用
+      channelDialogShow: false,
+      channelDialogId: "0001",
+      channelPinsShow: false,
+
+      channelInfo: {}
+    };
+  },
+
+  computed: {
+    //今いるパス(チャンネルID)を取得するだけ
+    getPath() {
+      return this.$route.params.id;
+    },
+
+    //ディスプレイのサイズから表示するボタンの要素のサイズを取得
+    getDisplaySize() {
+      switch (useDisplay().name.value) {
+        case "xs":
+          return "x-small";
+
+        case "sm":
+          return "small";
+
+        case "md":
+          return "default";
+
+        case "lg":
+          return "48";
+
+        case "xl":
+          return "large";
+
+        case "xxl":
+          return "96";
+
+        default:
+          return "small";
+      }
+    },
+
+    //スマホかどうかを返す
+    isMobile() {
+      return this.mobile;
+    }
+  },
+
+  methods: {
+    //チャンネルのミュート状態を切り替える
+    toggleMuteChannel() {
+      //チャンネルIDを予め取得しておく
+      let channelidHere = this.$route.params.id;
+
+      //チャンネルミュートリスト入っていたら削除、なかったら追加
+      if (this.LIST_NOTIFICATION_MUTE_CHANNEL.includes(channelidHere)) {
+        //ミュートリストからチャンネルを削除
+        this.LIST_NOTIFICATION_MUTE_CHANNEL.splice(
+          this.LIST_NOTIFICATION_MUTE_CHANNEL.indexOf(channelidHere),
+          1
+        );
+      } else {
+        //ミュートリストへ追加
+        this.LIST_NOTIFICATION_MUTE_CHANNEL.push(this.$route.params.id);
+      }
+
+      setCookie(
+        "configListMute",
+        this.LIST_NOTIFICATION_MUTE_CHANNEL.join("::"),
+        7
+      );
+    },
+  },
+
+  mounted() {
+    this.channelInfo = this.ChannelIndex[this.getPath];
+  }
+};
+</script>
+
+<template>
+
+  <!-- チャンネル設定ダイアログ -->
+  <ChannelConfig
+    v-if="channelDialogShow"
+    @closeDialog="channelDialogShow = false"
+    v-model="channelDialogShow"
+    :channelid="getPath"
+    :channelInfo="channelInfo"
+  />
+
+  <!-- チャンネルのピン留め表示ダイアログ -->
+  <ChannelPin
+    v-if="channelPinsShow"
+    v-model="channelPinsShow"
+    :pins="channelInfo.pins!==undefined?channelInfo.pins:[]"
+    :channelname="channelInfo.channelname"
+    :channelid="getPath"
+    @closePin="channelPinsShow=false"
+  />
+
+  <!-- ヘッダの表示部分(メイン) -->
+  <v-card
+    class="d-flex align-center justify-space-evenly rounded-0 px-1 pb-1 elevation-6"
+    style="max-width: 100%; height: 100%"
+  >
+    <!-- スマホUIだった時要のサイドバーボタン -->
+    <v-btn
+      v-if="isMobile"
+      @click="$emit('toggleSidebar')"
+      icon="mdi:mdi-menu-open"
+      class="rounded-lg ma-0"
+      variant="text"
+    >
+    </v-btn>
+    <!-- ヘッダ -->
+    <div
+      @click="channelDialogShow = true"
+      :ripple="isMobile"
+      :class="isMobile?'pa-2':null"
+      class="d-flex flex-column justify-start rounded-lg px-4"
+      variant="tonal"
+      style="width: 100%;"
+    >
+      <!-- チャンネル情報(チャンネル名、概要) -->
+      <div style="white-space: nowrap">
+        <div class="overflow-x-hidden text-truncate text-h5">
+          <span
+            v-if="channelInfo.scope === 'private'"
+            class="mdi mdi-lock"
+          ></span>
+          <v-chip v-if="channelInfo.previewmode" class="ma-1"
+            >プレビュー</v-chip
+          >
+
+          <!-- 発言にロール制限があったら -->
+          <span>
+            <!-- ホバーしたら表示するテキスト -->
+            <v-tooltip
+              activator="parent"
+              location="top center"
+              origin="top center"
+            >
+              {{ channelInfo.canTalk }}以上が発言可能
+            </v-tooltip>
+            <v-icon
+              v-if="channelInfo.canTalk !== 'Member' && channelInfo.canTalk !== undefined"
+              icon="mdi:mdi-circle-medium"
+              :color="channelInfo.canTalk === 'Admin' ? 'purple' : 'blue'"
+              size="small"
+            >
+            </v-icon>
+          </span>
+
+          {{ channelInfo.channelname }}
+        </div>
+      </div>
+
+      <v-divider v-if="!isMobile"></v-divider>
+
+      <!-- チャンネル概要 -->
+      <div
+        v-if="!isMobile"
+        color="grey"
+        class="rounded"
+      >
+        <p class="text-truncate text-h6">
+          {{ channelInfo.description }}
+        </p>
+      </div>
+    </div>
+
+    <!-- ボタン群 -->
+    <div class="d-flex align-center">
+      <!-- チャンネルの通知オン/オフボタン -->
+      <v-btn
+        v-if="!channelInfo.previewmode"
+        @click="toggleMuteChannel"
+        :size="getDisplaySize"
+        icon=""
+        class="rounded-lg ma-0"
+        variant="text"
+      >
+        <v-icon
+          v-if="!LIST_NOTIFICATION_MUTE_CHANNEL.includes($route.params.id)"
+          >mdi:mdi-bell</v-icon
+        >
+        <v-icon v-else>mdi:mdi-bell-off</v-icon>
+      </v-btn>
+
+      <!-- プレビュー時用のチャンネルブラウザに戻るボタン -->
+      <v-btn
+        v-if="channelInfo.previewmode"
+        @click="$router.push({ path: '/browser' })"
+        :size="getDisplaySize"
+        class="rounded-lg mx-1"
+        color="secondary"
+      >
+        ブラウザへ戻る
+      </v-btn>
+
+      <!-- ピン留め表示ボタン -->
+      <v-btn
+        v-if="!isMobile"
+        @click="() => (channelPinsShow = !channelPinsShow)"
+        :size="getDisplaySize"
+        icon=""
+        class="rounded-lg ma-0"
+        variant="text"
+      >
+        <v-icon>mdi:mdi-pin</v-icon>
+      </v-btn>
+
+      <!-- チャンネルメニューボタン -->
+      <v-btn
+        v-if="!isMobile"
+        @click="() => (channelDialogShow = !channelDialogShow)"
+        :size="getDisplaySize"
+        icon=""
+        class="rounded-lg ma-0"
+        variant="text"
+      >
+        <v-icon>mdi:mdi-menu</v-icon>
+      </v-btn>
+    </div>
+  </v-card>
+
+</template>
+
+<style scoped></style>
