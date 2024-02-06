828664989420460388c824e3a071809aa9b1309a
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-02-06 18:21:39 +0900

    [change] - バージョン情報の更新

--- Girack-v/src/data/socket.js
+++ Girack-v/src/data/socket.js
@@ -16,7 +16,7 @@ import { ref } from "vue";
 
 import { getCONFIG } from "../config.js";
 
-export const CLIENT_VERSION = "alpha_20240206";
+export const CLIENT_VERSION = "alpha_20240206_1";
 
 const {
   CONFIG_SYNC,
