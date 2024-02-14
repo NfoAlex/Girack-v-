badf99e8dbce55fc53a797b7aa3671082c8855aa
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-02-14 21:48:11 +0900

    [fix] - ホバーメニューの表示条件範囲の調整
    ガタガタをなくした

--- Girack-v/src/components/ChannelWindowComponent/ContentComponents/ContentRender.vue
+++ Girack-v/src/components/ChannelWindowComponent/ContentComponents/ContentRender.vue
@@ -337,6 +337,8 @@ export default {
       transition="none"
       location="top end"
       origin="start center"
+      @mouseover="mouseOverMsg(m.messageid, 'on')"
+      @mouseleave="mouseOverMsg(m.messageid, 'off')"
     >
       <!-- ホバーで反応する範囲 -->
       <template v-slot:activator="{ props }">
@@ -521,8 +523,6 @@ export default {
         <!-- ここからホバーメニュー -->
         <ContentHoverMenu
           v-if="!msgEditing"
-          @mouseover="mouseOverMsg(m.messageid, 'on')"
-          @mouseleave="mouseOverMsg(m.messageid, 'off')"
           @update-editing-message="msgEditing=true"
           @cancelEditing="msgEditing=false"
           style="z-index:30;"
