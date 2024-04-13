c62a4d643e5d1099ba05d3d8fc5c7d8adca43123
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-04-13 11:37:34 +0900

    [change] - バージョン情報の更新

--- Girack-v/src/components/MenuPageComponent/AboutGirack.vue
+++ Girack-v/src/components/MenuPageComponent/AboutGirack.vue
@@ -17,7 +17,7 @@ export default {
 <template>
   <div class="text-center pa-5" style="width:100%">
     <span>
-      <p class="text-h3">HoverIt</p>
+      <p class="text-h3">JustUpdates</p>
     </span>
     
     <br>
--- Girack-v/src/data/socket.js
+++ Girack-v/src/data/socket.js
@@ -16,7 +16,7 @@ import { ref } from "vue";
 
 import { getCONFIG } from "../config.js";
 
-export const CLIENT_VERSION = "alpha_20240215";
+export const CLIENT_VERSION = "alpha_20240413";
 
 const {
   CONFIG_SYNC,
