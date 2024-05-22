560b053b1a3868b01797e271ecfbeb8ea9867f8f
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2024-05-22 19:45:08 +0900

    [change] - メッセージの編集ツールを改善

--- Girack-v/src/components/ChannelWindowComponent/ContentComponents/ContentRender.vue
+++ Girack-v/src/components/ChannelWindowComponent/ContentComponents/ContentRender.vue
@@ -470,7 +470,7 @@ export default {
             <ContentMessageRender style="font-size:14px;" v-if="!msgEditing" :content="m.content" />
             <ContentEditing
               v-else
-              @close-editing="()=>{$emit('closeEditing'); msgEditing=false;}"
+              @close-editing="()=>{$emit('closeEditing')}"
               :channelid="m.channelid"
               :content="m.content"
               :messageid="m.messageid"
@@ -521,8 +521,8 @@ export default {
       <!-- ここからホバーメニュー -->
       <ContentHoverMenu
         v-if="!msgEditing"
-        @update-editing-message="msgEditing=true"
-        @cancelEditing="msgEditing=false"
+        @update-editing-message="$emit('editMessage',m.messageid)"
+        @cancelEditing="$emit('closeEditing')"
         style="z-index:30; width:fit-content;"
         :m="m"
         :userrole="getUserStats(m.userid, 'role')"
