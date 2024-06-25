a3b140102e7258ef56f11b5b88a263ebb31d1ac7
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2024-06-25 11:14:24 +0900

    [change] - バージョン情報（名前）の更新

--- Girack-v/src/components/MenuPageComponent/AboutGirack.vue
+++ Girack-v/src/components/MenuPageComponent/AboutGirack.vue
@@ -17,7 +17,7 @@ export default {
 <template>
   <div class="text-center pa-5" style="width:100%">
     <span>
-      <p class="text-h3">WeNeedDate</p>
+      <p class="text-h3">ItsJune</p>
     </span>
     
     <br>
